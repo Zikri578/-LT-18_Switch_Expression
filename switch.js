638cfba86d0562f0cb8b184ff7b414ccc214b1a9
MuhammadZikri <zikrim577@gmail.com>
2023-01-10 15:14:07 +0700

    Switch Javascript Expression

---
+++ switch.js
@@ -0,0 +1,14 @@
+// membuat variabel result 
+let result = 90;
+
+if (result <= 100) {
+    console.info("Istimewa");
+} else if (result <= 80) {
+    console.info("Sangat Bagus");
+} else if (result <= 60) {
+    console.info("Bagus");
+} else if (result <= 40) {
+    console.info("Tidak Bagus");
+} else {
+    console.info("Sangat Tidak Bagus");
+}
